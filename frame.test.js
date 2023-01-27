f8b95d771d491383fdf9ae0951bcaa234612f84a
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-27 11:45:06 +0000

    Implemented testing for validity of scores entered to Frame constructor

--- frame.test.js
+++ frame.test.js
@@ -5,17 +5,19 @@ const strikeFrame = new Frame(10);
 const spareFrame = new Frame(1, 9);
 const finalFrame = new Frame(10, 5, 10);
 
-describe("Constructor raises appropriate error for invalid frame scores", () => {
+describe("Constructor raises appropriate error for invalid arguments", () => {
   it("Frames cannot have no balls", () => {
     expect(() => {
       new Frame();
     }).toThrow(new Error("A frame must have at least one ball!"));
   });
+
   it("Frames cannot have only one ball if they are not strikes", () => {
     expect(() => {
       new Frame(9);
     }).toThrow(new Error("A frame with only one ball must be a strike!"));
   });
+
   it("Frames cannot have invalid data passed in", () => {
     expect(() => {
       new Frame(11, 5);
@@ -38,6 +40,18 @@ describe("Constructor raises appropriate error for invalid frame scores", () => 
         "Arguments of the Frame constructor must be integers between 1 and 10!"
       )
     );
+  });
+
+  it("rejects invalid Frame scores", () => {
+    expect(() => {
+      new Frame(2, 9);
+    }).toThrow(new Error("The score of a frame must be valid!"));
+    expect(() => {
+      new Frame(10, 1);
+    }).toThrow(new Error("The score of a frame must be valid!"));
+    expect(() => {
+      new Frame(1, 8, 5);
+    }).toThrow(new Error("The score of a frame must be valid!"));
   });
 });
 
