ce032f1ec9942374def250d6826e9729628f8070
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-29 12:26:16 +0000

    Added functionaility for adding frames to scorecard

--- scorecard.js
+++ scorecard.js
@@ -1,3 +1,29 @@
-class Scorecard {}
+class Scorecard {
+  constructor() {
+    this.frameCount = 0;
+  }
+
+  add(...frames) {
+    for (const frame of frames) {
+      if (this.frameCount === 10) {
+        throw new Error("Only 10 frames can be added to one scorecard!");
+      }
+      this.#updateLinkedListStructure(frame);
+      this.frameCount += 1;
+    }
+  }
+
+  calculate() {}
+
+  #updateLinkedListStructure(frame) {
+    if (this.frameCount === 0) {
+      this.firstFrame = frame;
+      this.lastFrame = frame;
+    } else {
+      this.lastFrame.next = frame;
+      this.lastFrame = frame;
+    }
+  }
+}
 
 module.exports = Scorecard;
